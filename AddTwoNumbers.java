bccfb16908fa18bc7de25e145b0adb022fc5b2fe
ketanuppal <ketanuppal@gmail.com>
2023-06-27 16:44:23 +0530

    add 2 numbers & data-types

---
+++ AddTwoNumbers.java
@@ -0,0 +1,28 @@
+//A program on add two numbers and some data-types in java
+public class AddTwoNumbers {
+    public static void main(String []args){
+        int a = 10;
+        int b = 25;
+        int c = a + b;
+        System.out.print(c);
+
+        //Data-Types
+        //for integers
+        int i = 10;  //4 bytes
+        short s = 15; //2 bytes
+        byte by = 11; //1 byte
+        long l = 117; //8 bytes
+
+        //for decimal numbers
+        double d = 10.5;  //8 bytes
+        float f = 120.25f;  //4 bytes (need to write f in the end)
+
+        //for characters
+        char ch = 'a'; //can only use single-quotes, double quotes are for a string!
+
+        //boolean data type
+        boolean bool1 = true;
+        boolean bool2 = false;
+    }
+}
+
