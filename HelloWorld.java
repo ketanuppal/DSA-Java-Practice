f5f08e3a8c8a7b10c580752bf84222e2d1fa4da0
ketanuppal <ketanuppal@gmail.com>
2023-06-27 15:38:36 +0530

    hello world program

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+        System.out.print("\n");
+        System.out.print("Hello, World!");
+        System.out.print("\n");
+        //OR (This is a comment statement)
+        System.out.print("Hello, World!\n");
+        System.out.print("Hello, World!\n");
+        //OR
+        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
+        /*
+        This
+        is
+        a
+        block
+        comment
+         */
+    }
+}
